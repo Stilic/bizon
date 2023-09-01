d59731f3165b317133d7434f31ccd2760c9c43e0
Stilic <lateman-jpeg@outlook.fr>
2023-09-01 12:56:20 +0200

    some stuff

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,9 +1,10 @@
+from __future__ import annotations
 from enum import Enum
 
 # HELPERS
 
 
-def make_error(message, line, col):
+def make_error(message: str, line: int, col: int):
     return Exception(f"{message} [{line}:{col}]")
 
 
@@ -26,12 +27,12 @@ class TokenType(Enum):
 class Token:
     def __init__(
         self,
-        type,
+        type: TokenType,
         value=None,
-        start_pos_line=None,
-        start_pos_col=None,
-        end_pos_line=None,
-        end_pos_col=None,
+        start_pos_line: int = None,
+        start_pos_col: int = None,
+        end_pos_line: int = None,
+        end_pos_col: int = None,
     ):
         self.type = type
         self.value = value
@@ -54,7 +55,7 @@ class Token:
         return str(self.type)
 
 
-def _check_character(char):
+def _check_character(char: str):
     return char != " " and char != "\t"
 
 
@@ -70,7 +71,7 @@ class Lexer:
 
         self.pos = -1
 
-    def make_token(self, type, value=None):
+    def make_token(self, type: TokenType, value=None):
         return Token(type, value, self.line, self.col)
 
     def advance(self):
@@ -88,7 +89,7 @@ class Lexer:
 
     def check_current_pos(self):
         if _check_character(self.code[self.pos]):
-            if self.code[self.pos].isdigit() or self.code[self.pos] == "-":
+            if self.code[self.pos].isdigit():
                 start_pos_line = self.line
                 start_pos_col = self.col
                 num = str(self.code[self.pos])
@@ -102,9 +103,7 @@ class Lexer:
                     else:
                         self.pos -= 1
                         break
-                if num == "-":
-                    self.tokens.append(self.make_token(TokenType.MINUS))
-                elif is_float:
+                if is_float:
                     self.tokens.append(
                         Token(
                             TokenType.FLOAT,
@@ -128,6 +127,8 @@ class Lexer:
                     )
             elif self.code[self.pos] == "+":
                 self.tokens.append(self.make_token(TokenType.PLUS))
+            elif self.code[self.pos] == "-":
+                self.tokens.append(self.make_token(TokenType.MINUS))
             elif self.code[self.pos] == "*":
                 self.tokens.append(self.make_token(TokenType.MULT))
             elif self.code[self.pos] == "/":
@@ -157,39 +158,55 @@ class Lexer:
 # NODES
 
 
-class Node:
-    def __init__(self, token):
+class NumberNode:
+    def __init__(self, token: Token):
         self.token = token
+
+        self.start_pos_line = token.start_pos_line
+        self.start_pos_col = token.start_pos_col
+        self.end_pos_col = token.end_pos_col
+        self.end_pos_line = token.end_pos_line
 
     def __repr__(self):
         return str(self.token)
 
 
-class BinOpNode(Node):
-    def __init__(self, left_node, token, right_node):
-        Node.__init__(self, token)
+class BinOpNode(NumberNode):
+    def __init__(self, left_node: NumberNode, token: Token, right_node: NumberNode):
+        NumberNode.__init__(self, token)
 
         self.left_node = left_node
         self.right_node = right_node
 
+        self.start_pos_line = left_node.start_pos_line
+        self.start_pos_col = left_node.start_pos_col
+        self.end_pos_col = right_node.end_pos_col
+        self.end_pos_line = right_node.end_pos_line
+
     def __repr__(self):
-        return f"({self.left_node}, {Node.__repr__(self)}, {self.right_node})"
+        return f"({self.left_node}, {NumberNode.__repr__(self)}, {self.right_node})"
 
 
-class UnaryOpNode(Node):
-    def __init__(self, token, node):
-        Node.__init__(self, token)
+class UnaryOpNode(NumberNode):
+    def __init__(self, token: Token, node: NumberNode):
+        NumberNode.__init__(self, token)
+
         self.node = node
 
+        self.start_pos_line = token.start_pos_line
+        self.start_pos_col = token.start_pos_col
+        self.end_pos_col = node.end_pos_col
+        self.end_pos_line = node.end_pos_line
+
     def __repr__(self):
-        return f"({Node.__repr__(self)}, {self.node})"
+        return f"({NumberNode.__repr__(self)}, {self.node})"
 
 
 # PARSER
 
 
 class Parser:
-    def __init__(self, tokens):
+    def __init__(self, tokens: list[Token]):
         self.tokens = tokens
 
     def advance(self):
@@ -206,7 +223,7 @@ class Parser:
             return UnaryOpNode(token, self.factor())
         elif token.type in (TokenType.INT, TokenType.FLOAT):
             self.advance()
-            return Node(token)
+            return NumberNode(token)
         elif token.type == TokenType.LPAREN:
             self.advance()
             expr = self.expr()
@@ -219,7 +236,7 @@ class Parser:
                     "Expected )", token.start_pos_line, token.start_pos_col
                 )
 
-    def bin_op(self, func, ops):
+    def bin_op(self, func, ops: list[TokenType]):
         left = func()
 
         while self.tokens[self.pos].type in ops:
@@ -256,17 +273,115 @@ class Parser:
         return expr
 
 
+# VALUES
+
+
+class Number:
+    def __init__(self, value: float):
+        self.value = value
+        self.set_pos()
+
+    def set_pos(
+        self,
+        start_pos_line: int = None,
+        start_pos_col: int = None,
+        end_pos_line: int = None,
+        end_pos_col: int = None,
+    ) -> Number:
+        self.start_pos_line = start_pos_line
+        self.start_pos_col = start_pos_col
+        self.end_pos_line = end_pos_line
+        self.end_pos_col = end_pos_col
+        return self
+
+    def added_to(self, other: Number) -> Number:
+        if isinstance(other, Number):
+            return Number(self.value + other.value)
+
+    def subbed_by(self, other: Number) -> Number:
+        if isinstance(other, Number):
+            return Number(self.value - other.value)
+
+    def multed_by(self, other: Number) -> Number:
+        if isinstance(other, Number):
+            return Number(self.value * other.value)
+
+    def dived_by(self, other: Number) -> Number:
+        if isinstance(other, Number):
+            return Number(self.value / other.value)
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+
+# INTERPRETER
+
+NEGATIVE_ONE = Number(-1)
+
+
+class Interpreter:
+    def visit(self, node: NumberNode) -> Number:
+        method_name = f"visit_{type(node).__name__}"
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node)
+
+    def no_visit_method(self, node):
+        raise Exception(f"No visit_{type(node).__name__} method defined")
+
+    def visit_NumberNode(self, node: NumberNode):
+        return Number(node.token.value).set_pos(
+            node.start_pos_line,
+            node.start_pos_col,
+            node.end_pos_line,
+            node.end_pos_col,
+        )
+
+    def visit_BinOpNode(self, node: BinOpNode):
+        left = self.visit(node.left_node)
+        right = self.visit(node.right_node)
+
+        if node.token.type == TokenType.PLUS:
+            left = left.added_to(right)
+        elif node.token.type == TokenType.MINUS:
+            left = left.subbed_by(right)
+        elif node.token.type == TokenType.MULT:
+            left = left.multed_by(right)
+        elif node.token.type == TokenType.DIV:
+            left = left.dived_by(right)
+
+        return left.set_pos(
+            node.start_pos_line,
+            node.start_pos_col,
+            node.end_pos_line,
+            node.end_pos_col,
+        )
+
+    def visit_UnaryOpNode(self, node: UnaryOpNode):
+        number = self.visit(node.node)
+
+        if node.token.type == TokenType.MINUS:
+            number = number.multed_by(NEGATIVE_ONE)
+
+        return number.set_pos(
+            node.start_pos_line,
+            node.start_pos_col,
+            node.end_pos_line,
+            node.end_pos_col,
+        )
+
+
 # RUN
 
 
 def run(code):
     # Make tokens
     tokens = Lexer(code).tokenize()
-    print(tokens)
 
     # Generate AST
     ast = Parser(tokens).parse()
-    print(ast)
+
+    interp = Interpreter()
+    return interp.visit(ast)
 
 
-run("-8 - 2.3")
+print(run("1+"))
